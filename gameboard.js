2f78e0d71a40a9dd56150a29a96e9335e6061c61
emthaa <110277522+emthaa@users.noreply.github.com>
2023-10-31 13:09:51 +0000

    fixed isPlacement function

--- gameboard.js
+++ gameboard.js
@@ -2,7 +2,7 @@ class Gameboard {
   constructor(size) {
     this.size = size;
     this.board = this.createGameboard();
-    this.missedShots = []
+    this.squaresShot = []
   }
 
   createGameboard() {
@@ -17,24 +17,32 @@ class Gameboard {
     return gameboardCellArray;
   }
 
-  isPlacement(x,y,length,isVertical){
-
-    if(isVertical){
-      if(y+length > this.size){
-        return false
-      }else{
-        return true
+  isPlacement(x, y, length, isVertical) {
+    if (isVertical) {
+      if (y + length > this.size) {
+        return false; // Check if the ship goes off the board vertically
       }
-    }else{
-      // horizontal
-      if(x+length > this.size){
-        return false
-      }else{
-        return true
+  
+      for (let i = 0; i < length; i++) {
+        if (x >= this.size || y + i >= this.size || this.board[x][y + i] !== false) {
+          return false; // Check for overlap or out-of-bounds vertically
+        }
       }
+      return true;
+    } else {
+      if (x + length > this.size) {
+        return false; // Check if the ship goes off the board horizontally
+      }
+  
+      for (let i = 0; i < length; i++) {
+        if (x + i >= this.size || y >= this.size || this.board[x + i][y] !== false) {
+          return false; // Check for overlap or out-of-bounds horizontally
+        }
+      }
+      return true;
     }
   }
-
+  
   placeShipHead(x, y, ship,isVertical) {
     // Logic to place a ship's head on the game board
     // You'd need to consider the ship's length, orientation, and validity of the placement
@@ -56,6 +64,7 @@ class Gameboard {
       }
     }
     
+    return true
 
   }
 
@@ -67,26 +76,60 @@ class Gameboard {
       
       this.board[x][y] = true
 
-      const missedCoordinates = {
+      const squareCoordinates = {
         x: x,
         y: y,
       }
 
-      this.missedShots.push(missedCoordinates)
+      this.squaresShot.push(squareCoordinates)
       return 'Miss!';
     } else {
       // sends the ‘hit’ function to the correct ship,
 
         this.board[x][y].hit()
         this.board[x][y] = true
-
+        const squareCoordinates = {
+          x: x,
+          y: y,
+        }
+  
+        this.squaresShot.push(squareCoordinates)
         return 'Hit!!'
     }
   }
 
- 
+  randomAttack(){
+
+    while (true){
+      let randX = Math.floor(Math.random() * this.size);
+      let randY = Math.floor(Math.random() * this.size);
+
+      if(this.squaresShot == []){ //if no shots hit yet
+        this.receiveAttack(randX,randY)
+      }
+
+      for(let i = 0; i < this.squaresShot.length;i++){ //check the squares that have been shot and compare to rand 
+        if(this.squaresShot[i].x == randX || this.squaresShot[i].y == randY){
+          break
+        }else{
+          this.receiveAttack(randX,randY)
+        }
+
+      }
+
+    }
+
+  }
+
+  printBoard(){
+    for (let i = 0; i < this.size; i++) {
+      console.log('\n')
+      for (let j = 0; j < this.size; j++) {
+        console.log(this.board[i][j]) 
+      }
+    }
+  }
 }
 
-// Example usage
 
 module.exports = Gameboard;
